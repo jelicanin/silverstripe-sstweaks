a7da7c63130d2ba498dbf226984ef81cfb30f120
Milan Jelicanin <milan.jelicanin@fractas.com>
2014-03-19 14:34:51 +0100

    added useful Bool options for positioning Pages on different Menus added methods for fetching different types of Menus added some universal func for fetching different filtering of Childrens added func for fetching PrevNextPage nav

--- code/extensions/SSTweaksSiteTree.php
+++ code/extensions/SSTweaksSiteTree.php
@@ -5,18 +5,22 @@
  * @author morven
  */
 class SSTweaksSiteTree extends DataExtension {
-    public static $db = array(
-        "SummaryContent"    => "Text",
-		"ShowChildren"      => "Boolean",
-		"ShowContact"       => "Boolean"
+
+    private static $db = array(
+        "SummaryContent" => "Text",
+		"ShowChildren" => "Boolean",
+		// "ShowContact" => "Boolean",
+        "ShowInTopMenus" => "Boolean",
+        "ShowInMainMenus" => "Boolean",
+        "ShowInFooterMenus" => "Boolean"
     );
     
-    public static $has_one = array(
-        "SummaryImage"      => "Image"
+    private static $has_one = array(
+        "SummaryImage" => "Image"
     );
 
     function updateCMSFields(FieldList $fields) {
-        $summary_fields = ToggleCompositeField::create('Summary', 'Summary Info.',
+        $summary_fields = ToggleCompositeField::create('Summary', 'Summary Info',
 			array(
 				TextareaField::create("SummaryContent", $this->owner->fieldLabel('SummaryContent')),
 				UploadField::create("SummaryImage", $this->owner->fieldLabel('SummaryImage'))
@@ -33,11 +37,83 @@ class SSTweaksSiteTree extends DataExtension {
 		
 	    $fields->addFieldToTab('Root.Settings', $children);
 	    
-        $contact = FieldGroup::create(
-	        CheckboxField::create('ShowContact', 'Show contact info on this page?')
-		)->setTitle('Contact Info (from settings)');
+		// $contact = FieldGroup::create(
+		// 	CheckboxField::create('ShowContact', 'Show contact info on this page?')
+		// )->setTitle('Contact Info (from settings)');
 		
-	    $fields->addFieldToTab('Root.Settings', $contact);
-    }
-}
+		// $fields->addFieldToTab('Root.Settings', $contact);
 
+        $fieldGroup = new FieldGroup(
+            new CheckboxField("ShowInTopMenus", "Top menu"), new CheckboxField("ShowInMainMenus", "Main menu"), new CheckboxField("ShowInFooterMenus", "Footer menu")
+        );
+
+        $fields->addFieldToTab("Root.Settings", $fieldGroup->setTitle('Where page will be shown?'));
+    }
+
+	
+	public function SortedChildren($num = 30) {
+    	$id = $this->owner->ID;
+        return DataObject::get("SiteTree", "ParentID = $id", "Created DESC", "", $num);
+    }
+
+    public function UnsortedChildren($num = 30) {
+    	$id = $this->owner->ID;
+        return DataObject::get("SiteTree", "ParentID = $id", "", "", $num);
+    }
+
+    public function UnsortedSiblings($num = 30) {
+    	$parent = $this->owner->ParentID;
+        return DataObject::get("SiteTree", "ParentID = $parent", "", "", $num);
+    }
+
+    public function SortedSiblings($num = 30) {
+    	$parent = $this->owner->ParentID;
+        return DataObject::get("SiteTree", "ParentID = $parent", "Created DESC", "", $num);
+    }
+
+    
+    public function PageById($id){
+        return SiteTree::get()->byID((int)$id);
+    }
+
+    public function GeneralMenu() {
+        return DataObject::get("SiteTree", "ShowInTopMenus != 1 AND ShowInFooterMenus != 1 AND ShowInMenus = 1 AND ParentID = 0");
+    }
+
+    public function MainMenu() {
+        return DataObject::get("SiteTree", "ShowInMainMenus = 1 AND ShowInMenus = 1 AND ParentID = 0");
+    }
+
+    public function TopMenu() {
+        return DataObject::get("SiteTree", "ShowInTopMenus = 1 AND ShowInMenus = 1 AND ParentID = 0");
+    }
+
+    public function FooterMenu() {
+        return DataObject::get("SiteTree", "ShowInFooterMenus = 1 AND ShowInMenus = 1 AND ParentID = 0");
+    }
+
+    public function PrevNextPage($mode = 'next') {
+    	$parent = $this->owner->ParentID;
+    	$sort = $this->owner->Sort;
+
+        if ($mode == 'next') {
+            $where = "ParentID = ($parent) AND Sort > ($sort)";
+            $sort = "Sort ASC";
+        } elseif ($mode == 'prev') {
+            $where = "ParentID = ($parent) AND Sort < ($sort)";
+            $sort = "Sort DESC";
+        } else {
+            return false;
+        }
+
+        $out = DataObject::get("SiteTree", $where, $sort, null, 1);
+
+        if ($out) {
+            return $out;
+        } else {
+            return false;
+        }
+    }
+
+
+}
